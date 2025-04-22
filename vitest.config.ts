29e8086967764b6c2621976c9ea2daff447fc122
Ko Nak-yean <59277499+nakyeonko3@users.noreply.github.com>
2025-04-22 22:20:10 +0900

    vitest 초기 설정
    vitest.config.ts 에서 react 플러그인 추가
    vitest.setup.ts 에서 next/navigation, next/cache 모킹 추가

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vitest/config';
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   test: {
-    environment: 'jsdom',
-    setupFiles: ['./vitest.setup.ts'],
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./vitest.setup.ts"],
   },
 });
--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,7 +1,10 @@
-import { afterEach } from 'vitest';
+import { afterEach, vi } from "vitest";
 
-import { cleanup } from '@testing-library/react';
+import { cleanup } from "@testing-library/react";
 
 afterEach(() => {
   cleanup();
 });
+
+vi.mock("next/cache");
+vi.mock("next/navigation");
